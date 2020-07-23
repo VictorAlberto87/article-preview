55312a5c85103f09eff809a13346520e12512955
Victor Marcas <victor11687@gmail.com>
2020-07-23 09:12:50 -0500

    Init project

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+(function () {
+    const share = document.getElementById('share');
+    const shareSocial = document.getElementById('share-social');
+    
+    share.addEventListener('click', (e) => {
+
+    })
+})
