95843fc36d1d5f097565975361212861b2b8c4ac
Victor Marcas <victor11687@gmail.com>
2020-07-25 22:49:58 -0500

    Actualización de eventos
    Se ha agregado validación para el overlay

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,14 @@
     const shareSocial = document.getElementById('share-social');
     
     share.addEventListener('click', (e) => {
-
+        e.preventDefault();
+        console.log(share.classList.contains('active'));
+        if(!share.classList.contains('active')) {
+            shareSocial.classList.add('active');
+            share.classList.add('active');
+        }else{
+            shareSocial.classList.remove('active');
+            share.classList.remove('active');
+        }
     })
-})
+})();
